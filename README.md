bb295994b5708fd7bd43c8cf15497e6ed237ad97
Ajay kumar <ajcoder25@gmail.com>
2024-09-12 18:02:32 +0530

    Count App

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS-Mini-Projects
+ This repository contains a collection of mini projects designed to help developers practice and enhance their JavaScript skills
---
+++ style.js
@@ -0,0 +1,31 @@
+const minusEl = document.getElementById('minus');  // Corrected
+const plusEl = document.getElementById('plus');    // Corrected
+const resultEl = document.getElementById('Result');
+const changeEl = document.getElementById('change')
+
+
+let counter = 0;
+
+const adding = () => {
+    resultEl.innerText = counter;
+}
+
+
+///minus
+
+minusEl.addEventListener('click', () => {
+    const value = Number(changeEl.value)
+    counter -= value
+    adding()
+    console.log(counter)
+})
+
+plusEl.addEventListener('click', () => {
+    const value = Number(changeEl.value)
+    counter += value
+    adding()
+
+    console.log(counter)
+})
+
+
